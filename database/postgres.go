9eb03358816e57e4c1e5b14336b09560282926f9
lautaro strappazzon <juan.strappazzon@kdsoft.io>
2023-07-26 21:24:08 -0300

    postgres repository implementation, docker compose and file

---
+++ database/postgres.go
@@ -0,0 +1,49 @@
+package database
+
+import (
+	"context"
+	"database/sql"
+
+	_ "github.com/lib/pq"
+
+	"github.com/lautistr/social-pet/models"
+)
+
+type PostgresRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresRepository(url string) (*PostgresRepository, error) {
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	return &PostgresRepository{db}, nil
+}
+
+func (repo *PostgresRepository) Close() {
+	repo.db.Close()
+}
+
+func (repo *PostgresRepository) InsertPost(ctx context.Context, post *models.Post) error {
+	_, err := repo.db.ExecContext(ctx, "INSERT INTO posts (id, body, created_at) VALUES ($1, $2)", post.ID, post.Body)
+	return err
+}
+
+func (repo *PostgresRepository) ListPosts(ctx context.Context) ([]*models.Post, error) {
+	rows, err := repo.db.QueryContext(ctx, "SELECT id, body, created_at FROM posts")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	posts := []*models.Post{}
+	for rows.Next() {
+		post := &models.Post{}
+		if err := rows.Scan(&post.ID, &post.Body, &post.CreatedAt); err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
+	}
+	return posts, nil
+}
