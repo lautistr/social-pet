6c2a49fef2b497a3ae78b1a220a11eae739f6040
lautaro strappazzon <juan.strappazzon@kdsoft.io>
2023-07-27 08:11:56 -0300

    nats mb implementation

---
+++ events/nats.go
@@ -0,0 +1,94 @@
+package events
+
+import (
+	"bytes"
+	"context"
+	"encoding/gob"
+
+	"github.com/lautistr/social-pet/models"
+	"github.com/nats-io/nats.go"
+)
+
+type NatsEventStore struct {
+	conn            *nats.Conn
+	postCreatedSub  *nats.Subscription
+	postCreatedChan chan CreatedPostMessage
+}
+
+func NewNats(url string) (*NatsEventStore, error) {
+	conn, err := nats.Connect(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return &NatsEventStore{
+		conn: conn,
+	}, nil
+}
+
+func (n *NatsEventStore) Close() {
+	if n.conn != nil {
+		n.conn.Close()
+	}
+	if n.postCreatedSub != nil {
+		n.postCreatedSub.Unsubscribe()
+	}
+	close(n.postCreatedChan)
+}
+
+func (n *NatsEventStore) encodeMessage(m Message) ([]byte, error) {
+	b := bytes.Buffer{}
+	err := gob.NewEncoder(&b).Encode(m)
+	if err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+func (n *NatsEventStore) PublishCreatedPost(ctx context.Context, post *models.Post) error {
+	msg := CreatedPostMessage{
+		Post: post,
+	}
+
+	data, err := n.encodeMessage(msg)
+	if err != nil {
+		return err
+	}
+	return n.conn.Publish(msg.Type(), data)
+}
+
+func (n *NatsEventStore) decodeMessage(data []byte, m interface{}) error {
+	b := bytes.Buffer{}
+	b.Write(data)
+	return gob.NewDecoder(&b).Decode(m)
+}
+
+func (n *NatsEventStore) OnCreatedPost(ctx context.Context, f func(CreatedPostMessage)) (err error) {
+	msg := CreatedPostMessage{}
+	n.postCreatedSub, err = n.conn.Subscribe(msg.Type(), func(m *nats.Msg) {
+		n.decodeMessage(m.Data, &msg)
+		f(msg)
+	})
+	return
+}
+
+func (n *NatsEventStore) SubscribeCreatedPost(ctx context.Context) (<-chan CreatedPostMessage, error) {
+	m := CreatedPostMessage{}
+	n.postCreatedChan = make(chan CreatedPostMessage, 64)
+	ch := make(chan *nats.Msg, 64)
+	var err error
+	n.postCreatedSub, err = n.conn.ChanSubscribe(m.Type(), ch)
+	if err != nil {
+		return nil, err
+	}
+	go func() {
+		for {
+			select {
+			case msg := <-ch:
+				n.decodeMessage(msg.Data, &m)
+				n.postCreatedChan <- m
+			}
+		}
+	}()
+	return (<-chan CreatedPostMessage)(n.postCreatedChan), nil
+}
